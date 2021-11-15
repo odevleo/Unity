db513d419485014ab1661079ab014a021173e500
odevleo <leobragab@hotmail.com>
2021-11-15 18:23:07 -0300

    update

---
+++ Scripts/cameras.cs
@@ -0,0 +1,9 @@
+// Dois tipos de cameras: ortográfica ou perspectiva
+
+// Em jogos 2D, normalmente se utiliza da camera ortográfica
+
+// Na camera ortográfica não existe a profundidade dos objetos na cena, ou seja, a posição z não importa
+
+// Camera pespectiva é utilizada pra jogos 3D
+
+// Se o jogo for pixelart, desabilitar o Anti Analisy (MSAA) e o HDR
---
+++ Scripts/update_fixedUpdate.cs
@@ -0,0 +1,24 @@
+// void Start() : acontece imediatamente antes do primeiro frame do jogo
+// usado para inicializar os GetComponets que serão usados no Update
+// executar sempre antes do update
+
+
+
+// void Awake() : utilizado caso precise executar algo antes do Start
+
+
+// void OnEnable() : usado para habilitar algum objeto desabilitado
+
+
+
+// void Update() : Função de loop, acontece a cada frame por segundo,
+// possui uma taxa de atualização fixa
+// responsável pela lógica do jogo
+
+
+
+// void FixedUpdate() : todos os comandos referentes a física colocamos aqui
+
+
+
+// void LateUpdate() : acontece após o Update, utilizado para controle de camera, controle de animações, etc
